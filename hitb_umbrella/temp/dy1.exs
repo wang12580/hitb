791e3b87bd99b0e224b791693c3dcb6d48dcca87
dingyun <dingyun@hitb.com.cn>
2018-03-29 14:47:28 +0800

    mnesia

--- hitb_umbrella/temp/dy1.exs
+++ hitb_umbrella/temp/dy1.exs
@@ -1,6 +1,14 @@
+# https://elixirschool.com/en/lessons/specifics/mnesia/
+# http://www.culttt.com/2016/10/12/working-mnesia-elixir/
 IO.puts("======mnesia test=====")
 :mnesia.start()
 # :mnesia.info()
+# t1 = fn -> :mnesia.table(:block_chain) end
+# t1 = fn -> :mnesia.table_info(:block_chain, :all) end
+# t1 = fn -> :mnesia.wait_for_tables([:block_chain], 1000) end
+# t1 = fn -> :mnesia.schema(:block_chain) end
+# IO.inspect t1.()
+
 
 # block = %{
 #   index: 0,
@@ -16,14 +24,32 @@ IO.puts("======mnesia test=====")
 #   block.data,
 #   block.hash}) end
 
-# t1 = fn -> :mnesia.table(:block_chain) end
-# t1 = fn -> :mnesia.table_info(:block_chain, :all) end
-
-# t1 = fn -> :mnesia.wait_for_tables([:block_chain], 1000) end
-# t1 = fn -> :mnesia.schema(:block_chain) end
 
 
+t1 = fn ->
+  :ok = :mnesia.wait_for_tables([:block_chain], 5000)
 
-IO.inspect t1.()
-# result = :mnesia.transaction(t1)
-# IO.inspect result
+  r1 = :mnesia.read({:block_chain, 1})
+
+  r2 = :mnesia.match_object({:block_chain, 1, :_, :_, :_, :_})
+
+  r3 = :mnesia.select(:block_chain, [{{:block_chain, :"$1", :"$2", :"$3", :"$4", :"$5"}, [{:>, :"$1", 0}], [:"$$"]}])
+
+  r4 = :mnesia.first(:block_chain)
+  r5 = :mnesia.last(:block_chain)
+  r6 = :mnesia.next(:block_chain, 0)
+  r7 = :mnesia.prev(:block_chain, 1)
+  r8 = :mnesia.all_keys(:block_chain)
+
+  [r1, r2, r3, r4, r5, r6, r7, r8]
+end
+
+result1 = :mnesia.transaction(t1)
+IO.inspect result1
+
+
+
+# t2 = fn ->
+#   :mnesia.add_table_index(:block_chain, :timestamp)
+# end
+# IO.inspect t2.()
