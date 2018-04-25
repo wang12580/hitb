43a6c86d38fcaddbf782ad2a0fb82d975751586e
wang12580 <1258093916@qq.com>
2018-04-25 17:23:17 +0800

    修改hitb基础分析chart图显示问题 增加server 和stat测试代码

--- hitb_umbrella/apps/stat/priv/repo/migrations/20180319030754_create_client_stat.exs
+++ hitb_umbrella/apps/stat/priv/repo/migrations/20180319030754_create_client_stat.exs
@@ -3,7 +3,7 @@ defmodule Hospitals.Repo.Migrations.CreateClientStat do
 
   def change do
     create table(:client_stat) do
-      add :username, :integer #创建用户
+      add :username, :string #创建用户
       add :filename, :string #文件名
       add :data, :string #数据
       timestamps()
