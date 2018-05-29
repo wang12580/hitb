8d760e9798161a3b1396f278523439011c0d99e6
wang12580 <1258093916@qq.com>
2018-05-29 10:27:12 +0800

    修复部分页面不显示问题 user增加字段

--- hitb_umbrella/apps/hitb/priv/repo/migrations/20171105531102_create_user.exs
+++ hitb_umbrella/apps/hitb/priv/repo/migrations/20171105531102_create_user.exs
@@ -11,6 +11,7 @@ defmodule Hitb.Server.Repo.Migrations.CreateUser do
       add :tel, :string #电话
       add :username, :string #用户名
       add :type, :integer #用户类型 1-管理员 2-普通用户
+      add :hash, :string #区块链hash值
       add :key, {:array, :string}
       add :is_show, :boolean, default: false
       timestamps()
