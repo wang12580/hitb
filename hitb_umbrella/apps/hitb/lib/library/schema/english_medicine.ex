ff1652d4a95d36abc9a22c641b8e0e8a681b4ec2
wang12580 <1258093916@qq.com>
2018-06-04 17:01:11 +0800

    增加西药查询

---
+++ hitb_umbrella/apps/hitb/lib/library/schema/english_medicine.ex
@@ -0,0 +1,25 @@
+defmodule Hitb.Library.EnglishMedicine do
+    use Ecto.Schema
+    import Ecto.Changeset
+    alias Hitb.Library.EnglishMedicine
+  
+    schema "english_medicine" do
+      field :first_level, :string #一级分类
+      field :second_level, :string #二级分类
+      field :third_level, :string #三级分类
+      # add :icd9_aa, {:array, :string}
+      field :zh_name, :string #中文名称
+      field :en_name, :string # 英文名称
+      field :dosage_form, :string #剂型
+      field :reimbursement_restrictions, :string #报销限制内容
+  
+      timestamps()
+    end
+  
+  
+    def changeset(%EnglishMedicine{} = english_medicine, attrs) do
+      english_medicine
+      |> cast(attrs, [:first_level, :second_level, :third_level, :zh_name, :en_name, :dosage_form, :reimbursement_restrictions])
+      |> validate_required([:first_level, :second_level, :third_level, :zh_name, :en_name, :dosage_form])
+    end
+  end
