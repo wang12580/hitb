ce48821e87658aee5ee8b6ae06d74068bb156c29
duanzhichao <duanzhichao2008@gmail.com>
2018-06-05 09:25:47 +0800

    修复登录bug

--- hitb_umbrella/apps/server/lib/service/user_service.ex
+++ hitb_umbrella/apps/server/lib/service/user_service.ex
@@ -43,7 +43,7 @@ defmodule Server.UserService do
               if(db_user.block_address == nil)do
                 %{publicKey: "", username: ""}
               else
-                AccountService.getAccountByAddress()
+                AccountService.getAccountByAddress(db_user.block_address)
               end
             blockchain = %{address: db_user.block_address, publicKey: account.publicKey, secret: account.username}
             [true, %{id: db_user.id, org: db_user.org, login: true, username: db_user.username, type: db_user.type, key: db_user.key, blockchain: blockchain, is_show: db_user.is_show}]
