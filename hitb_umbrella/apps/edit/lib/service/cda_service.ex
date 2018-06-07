402b3a57743719a8543c836fe10c6f6db7faf75d
wangzedong <1258093916@qq.com>
2018-06-07 17:19:46 +0800

    增加cdafile表 增加libraryfile表 修改clinet远程查询方式

--- hitb_umbrella/apps/edit/lib/service/cda_service.ex
+++ hitb_umbrella/apps/edit/lib/service/cda_service.ex
@@ -4,13 +4,14 @@ defmodule Edit.CdaService do
   alias Hitb.Repo, as: HitbRepo
   alias Block.Repo, as: BlockRepo
   alias Hitb.Edit.Cda, as: HitbCda
+  alias Hitb.Edit.CdaFile, as: HitbCdaFile
   alias Block.Edit.Cda, as: BlockCda
   alias Hitb.Edit.MyMould
   alias Hitb.Time
   alias Hitb.Server.User
 
   def cda_user() do
-    users = HitbRepo.all(from p in HitbCda, select: p.username)|>:lists.usort
+    users = HitbRepo.all(from p in HitbCdaFile, select: p.username)|>:lists.usort
     users2 = HitbRepo.all(from p in User, where: p.is_show == false, select: p.username)
     users2 = if(users2)do users2 else [] end
     [users -- users2, "读取成功"]
