58aa054183c5be224bbf4de0ff54d893010ea3d5
duanzhichao <duanzhichao2008@gmail.com>
2018-07-30 14:32:52 +0800

    增加用户主动下线功能

--- hitb_umbrella/apps/hitbserver_web/lib/hitbserver_web/channels/online_channel.ex
+++ hitb_umbrella/apps/hitbserver_web/lib/hitbserver_web/channels/online_channel.ex
@@ -45,6 +45,12 @@ defmodule HitbserverWeb.OnlineChannel do
     {:noreply, socket}
   end
 
+  def handle_in("用户下线", %{"username" => username}, socket) do
+    Hitb.ets_insert(:socket_user, username, false)
+    Logger.warn("用户--#{username}--用户下线")
+    {:noreply, socket}
+  end
+
   def handle_in("邀请加入" <> _private_room_id, %{"body" => body, "username" => username, "create_room_time" => create_room_time, "invite" => invite, "room_owner" => room_owner}, socket) do
     broadcast! socket, "邀请加入", %{message: "#{username}邀请您进入#{body}房间", time: Hitb.Time.standard_time(), room: body, create_room_time: create_room_time, invite: invite, room_owner: room_owner}
     {:noreply, socket}
